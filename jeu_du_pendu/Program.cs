a78c1d9924a3f1919b0f07dfe86490541d11468d
Charlyn Lonchampt <charlyn.lonchampt@gmail.com>
2023-09-10 18:07:51 +0200

    1er commit

---
+++ jeu_du_pendu/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Transactions;
+
+namespace jeu_du_pendu
+{
+    class Program
+    {
+
+        static void AfficherMot(string mot, List<char> lettres)
+        {
+            // _ _ _ _ _ _ _ _
+            for (int i = 0; i < mot.Length; i++)
+            {
+                if (lettres.Contains(mot[i]))
+                {
+                    Console.Write(mot[i] + " ");
+                }
+                else
+                {
+                    Console.Write("_ ");
+                }
+                
+            }
+            Console.WriteLine();
+        }
+
+        static char DemanderUneLettre()
+        {
+            // Rentrez une lettre
+            // ERREUR : vous devez rentrez une seule lettre
+            // retrun -> majuscules (ToUpper)
+            
+            while (true)
+            {
+                Console.Write("Rentrez une lettre : ");
+                string reponse = Console.ReadLine();
+                reponse = reponse.ToUpper();
+                try
+                {
+                    char lettre = char.Parse(reponse);
+                    return lettre;
+                }
+                catch
+                {
+                    Console.WriteLine("ERREUR : vous devez rentrez une seule lettre");
+                }
+            }
+            
+
+        }
+
+        static void DevinerMot(string mot)
+        {
+            ///
+
+            // _ _ _ _
+            // E _ _ E _ _
+            AfficherMot(mot, new List<char> { 'E','P'});
+
+            DemanderUneLettre();
+        }
+        static void Main(string[] args)
+        {
+            string mot = "ELEPHANT";
+            DevinerMot(mot);
+
+        }
+    }
+}
